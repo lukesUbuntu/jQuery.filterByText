fb7c1c9238911271bd46c4229ba46a92ffe140af
lukesUbuntu <luke@hardiman.co.nz>
2015-12-02 21:22:06 +1300

    workign filter with added text input

--- jQueryfilterByText.js
+++ jQueryfilterByText.js
@@ -1,31 +1,34 @@
- jQuery.fn.filterByText = function () {
-  //loop all selected elements
-    $.each(this,function (i,e) {
-     var $selectBx = $(e);
-	 //create out inputbox
-	 var textInput = $('<input style="position: absolute;" type="text">')
-	  //get all options and store
-	  var dOptions = [];
-	  $selectBx.find('option').each(function () {
+jQuery.fn.filterByText = function () {
+        //loop all selected elements
+        $.each(this,function (i,e) {
+            var $selectBx = $(e).wrap("<div></div>");
+            //$selectBx = $selectBx.wrap("<div></div>");
+            //create out inputbox
+            var textInput = $('<input style="position: absolute;" type="text">')
+            //get all options and store
+            var dOptions = [];
+            $selectBx.find('option').each(function () {
                 dOptions.push({value: $(this).val(), text: $(this).text()});
-      });
-	  //store as data option
-	  $selectBx.data('dOptions', dOptions);
-	  //on keychange update
-	   $(textInput).bind('change keyup', function () {
-			//regex search and show only
-			var search = $(this).val().trim();
-			//get options and clear
-		    var dOptions = $(selectBx).empty().data('dOptions');
+            });
+            //store as data option
+            $selectBx.data('dOptions', dOptions);
+            //on keychange update
+            $(textInput).bind('change keyup', function () {
+                //regex search and show only
+                var search = $(this).val().trim();
+                //get options and clear
+                var dOptions = $selectBx.empty().data('dOptions');
                 var regex = new RegExp(search, "gi");
                 $.each(dOptions, function (i) {
-                    var option = options[i];
-                    if (option.text.test(regex)) {
-                        $(selectBx).append(
+                    var option = dOptions[i];
+                     if (option.text.match(regex) !== null) {
+                        $($selectBx).append(
                             $('<option>').text(option.text).val(option.value)
                         );
                     }
                 });
-	   });
-    })
- });
+            });
+
+            $selectBx.parent().prepend(textInput);
+        })
+    };
