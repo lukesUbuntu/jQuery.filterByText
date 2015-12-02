f82fd6d04a05ddbf15d5cf2271c3d0fd9e205dfd
lukesUbuntu <luke@hardiman.co.nz>
2015-12-02 21:07:48 +1300

    added loopin options in selectbox

--- jQueryfilterByText.js
+++ jQueryfilterByText.js
@@ -2,5 +2,12 @@
   //loop all selected elements
     $.each(this,function (i,e) {
      var $selectBx = $(e);
+	  //get all options and store
+	  var dOptions = [];
+	  $selectBx.find('option').each(function () {
+                dOptions.push({value: $(this).val(), text: $(this).text()});
+      });
+	  //store as data option
+	  $selectBx.data('dOptions', dOptions);
     })
  });
