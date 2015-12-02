98e9aa435ca197a51af305c6e8711f317c3b9cc7
lukesUbuntu <luke@hardiman.co.nz>
2015-12-02 21:10:47 +1300

    Added searchbox

--- jQueryfilterByText.js
+++ jQueryfilterByText.js
@@ -2,6 +2,8 @@
   //loop all selected elements
     $.each(this,function (i,e) {
      var $selectBx = $(e);
+	 //create out inputbox
+	 var textInput = $('<input style="position: absolute;" type="text">')
 	  //get all options and store
 	  var dOptions = [];
 	  $selectBx.find('option').each(function () {
@@ -9,5 +11,10 @@
       });
 	  //store as data option
 	  $selectBx.data('dOptions', dOptions);
+	  //on keychange update
+	   $(textInput).bind('change keyup', function () {
+		   //get options and clear
+		    var dOptions = $(selectBx).empty().data('dOptions');
+	   });
     })
  });
